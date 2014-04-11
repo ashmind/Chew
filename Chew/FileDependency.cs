6ac9d4f1d94eb74a08d85602763ab70fdee04cbb
Andrey Shchekin <ashmind@gmail.com>
2014-04-11 22:47:55 +1200

    Minor improvement.

---
+++ Chew/FileDependency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Chew {
+    public class FileDependency {
+        public FileDependency([NotNull] string content, [NotNull] Action<string> providePath) {
+            this.Content = Argument.NotNullOrEmpty("content", content);
+            this.ProvidePath = Argument.NotNull("providePath", providePath);
+        }
+
+        [NotNull] public string Content { get; private set; }
+        [NotNull] public Action<string> ProvidePath { get; private set; }
+    }
+}
